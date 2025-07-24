2fff1a035635c61888a0e49fbe63ddb1f31b6ede
sandymoh <sandymohammad99@gmail.com>
2025-07-24 19:43:34 +0700

    feat: hero update style and dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import Hero from "../components/Hero";
+import Hero from '../components/Hero';
 
 export default function Home() {
   return (
-    <div className="font-sans">
-      <main>
+    <div className="flex flex-col font-sans">
+      <main className="flex-1 pt-4">
         <Hero />
+        {/* <Hero /> */}
+        {/* <Hero /> */}
       </main>
     </div>
   );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,25 +11,28 @@ const Hero: React.FC = () => {
   ];
 
   return (
-    <section className="container mx-auto h-screen px-4 py-12 sm:px-6 lg:px-8">
-      <div className="flex h-full">
-        <div className="flex h-full w-fit flex-col justify-center text-right">
-          <h6 className="font-handwriting text-3xl">
+    <section className="container mx-auto h-full px-4 py-12 sm:px-6 lg:px-8">
+      <div className="flex h-full items-center justify-center gap-4">
+        <div className="flex w-fit flex-col justify-center text-right">
+          <h6 className="font-handwriting text-lg md:text-2xl lg:text-3xl">
             I Code and Design just for you
           </h6>
-          <h1 className="text-sandy">Sandy</h1>
+          <h1 className="text-sandy-mobile md:text-sandy">Sandy</h1>
           <div className="flex w-full justify-end gap-4">
-            <div className="flex h-60 flex-col justify-end gap-1">
+            <div className="flex h-40 flex-col justify-end gap-1 md:h-48 lg:h-60">
               {services.map((service, index) => (
-                <h6 key={index} className="font-handwriting text-2xl font-bold">
+                <h6
+                  key={index}
+                  className="font-handwriting text-base font-bold md:text-xl lg:text-2xl"
+                >
                   {service}
                 </h6>
               ))}
             </div>
-            <h2 className="text-mo h-fit">Mo.</h2>
+            <h2 className="text-mo-mobile md:text-mo h-fit">Mo.</h2>
           </div>
         </div>
-        <div className="flex h-full w-full items-center justify-center">
+        <div className="hidden w-fit items-center justify-center md:flex">
           <Image
             src="/images/hero/dummy.jpg"
             alt="Sandy Mo"
