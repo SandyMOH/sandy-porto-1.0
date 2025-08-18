983e71786aeb0785105f039b384249dc4fe81081
sandymoh <sandymohammad99@gmail.com>
2025-08-18 16:22:23 +0700

    faet: add header and subhero

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Hero from '../components/Hero/Hero';
+import Header from '../components/Header/Header';
+import SubHero from '../components/SubHero/SubHero';
 
 export default function Home() {
   return (
     <div className="flex flex-col font-sans">
-      <main className="flex-1 pt-4">
+      <Header />
+      <main className="flex-1 pt-16 md:pt-8">
         <Hero />
+        <SubHero />
       </main>
     </div>
   );
