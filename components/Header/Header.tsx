f5f633309b48d7584d536f170647c1d9e99f710f
sandymoh <sandymohammad99@gmail.com>
2025-09-03 22:01:27 +0700

    feat: make header max-width and make component center

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -4,10 +4,12 @@ import Menu from './components/Menu';
 
 const Header: React.FC = () => {
   return (
-    <header className="fixed top-0 left-0 z-50 flex w-full justify-between p-4">
-      <JakartaTime />
-      <Sandy />
-      <Menu />
+    <header className="fixed top-0 left-0 z-50 w-full">
+      <div className="container mx-auto flex items-center justify-between px-4 py-3">
+        <JakartaTime />
+        <Sandy />
+        <Menu />
+      </div>
     </header>
   );
 };
