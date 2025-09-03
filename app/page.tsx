c9dd08f27ac260fa0d8a6906bfe38dfd9e13e396
sandymoh <sandymohammad99@gmail.com>
2025-09-03 22:02:06 +0700

    feat: create default screen

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,11 @@ import Hero from '../components/Hero/Hero';
 import Header from '../components/Header/Header';
 import SubHero from '../components/SubHero/SubHero';
 import About from '@/components/About/About';
-import AboutMobile from '@/components/About/AboutMobile';
+import Footer from '@/components/Footer/Footer';
 
 export default function Home() {
   return (
-    <div className="flex flex-col font-sans">
+    <div className="flex min-h-screen w-screen flex-col font-sans">
       <Header />
       <main className="flex-1 overflow-x-hidden pt-16 md:pt-8">
         <Hero />
