602bd8b4f73b9c49d7a8654cf9ff81f1c4743101
sandymoh <sandymohammad99@gmail.com>
2025-09-05 14:20:34 +0700

    feat: add max width to header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -5,7 +5,7 @@ import Menu from './components/Menu';
 const Header: React.FC = () => {
   return (
     <header className="fixed top-0 left-0 z-50 w-full">
-      <div className="container mx-auto flex items-center justify-between px-4 py-3">
+      <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-3">
         <JakartaTime />
         <Sandy />
         <Menu />
