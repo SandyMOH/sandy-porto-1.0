0e90a4ed9378076431b325190871812b1c4fc560
sandymoh <sandymohammad99@gmail.com>
2025-09-09 12:10:12 +0700

    refactor: add useState and useEffect

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,17 +1,21 @@
 'use client';
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useMediaQuery } from '@/libs/hooks/useMediaQuery';
 
 import AboutMobile from './AboutMobile';
 import AboutDesktop from './AboutDesktop';
 
 export default function About() {
+  const [desktop, setDesktop] = useState(true);
+
   const isDesktop = useMediaQuery('(min-width: 1250px)');
 
+  useEffect(() => {
+    setDesktop(isDesktop);
+  }, [isDesktop]);
+
   return (
-    <section id="about">
-      {isDesktop ? <AboutDesktop /> : <AboutMobile />}
-    </section>
+    <section id="about">{desktop ? <AboutDesktop /> : <AboutMobile />}</section>
   );
 }
