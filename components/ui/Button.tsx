dc90e846b83c27903f2b1edfa70c149c087bc0c1
sandymoh <sandymohammad99@gmail.com>
2025-09-09 11:53:10 +0700

    feat: add size to button

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -7,18 +7,32 @@ import { useGSAP } from '@gsap/react';
 
 interface ButtonProps {
   className?: string;
+  size?: 'small' | 'medium' | 'large';
+  children: React.ReactNode;
 }
 
-const Button: React.FC<ButtonProps> = ({ className }) => {
+const Button: React.FC<ButtonProps> = ({
+  className,
+  size = 'medium',
+  children,
+}) => {
   const onClick = () => {};
+
+  const sizeStyles = {
+    small: 'px-3 py-1 text-sm',
+    medium: 'px-3 py-1 text-base', // text-base is usually the default
+    large: 'px-4 py-3 text-lg',
+  };
 
   return (
     <button
       className={`${className} text-pink group relative cursor-pointer rounded-full bg-gradient-to-r from-pink-400 via-pink-600 to-pink-400 p-[2px] transition-colors duration-300 hover:text-white`}
       onClick={onClick}
     >
-      <span className="bg-background block rounded-full px-3 py-1 transition-colors duration-300 group-hover:bg-black">
-        Button
+      <span
+        className={`${sizeStyles[size]} bg-background block rounded-full transition-colors duration-300 group-hover:bg-black`}
+      >
+        {children}
       </span>
     </button>
   );
