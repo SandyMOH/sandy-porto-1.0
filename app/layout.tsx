11c8b84216fe6003c505797c9f161860ab5c7c64
sandymoh <sandymohammad99@gmail.com>
2025-09-11 21:29:54 +0700

    feat: make web default mode is dark

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,6 +39,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="color-scheme" content="dark" />
+      </head>
       <body
         className={`${fontSyne.variable} ${fontArchitectsDaughter.variable} antialiased`}
       >
